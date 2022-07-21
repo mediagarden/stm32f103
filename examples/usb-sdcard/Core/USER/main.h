4407f46f9513f312aac0e7af6ac5244b76f27f2f
zimengyu2020 <649822851@qq.com>
2022-07-21 21:18:28 +0800

    add usb-sdcard

---
+++ examples/usb-sdcard/Core/USER/main.h
@@ -0,0 +1,49 @@
+/**
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+  /* Private includes ----------------------------------------------------------*/
+
+  /* Exported types ------------------------------------------------------------*/
+
+
+  /* Exported constants --------------------------------------------------------*/
+
+  /* Exported macro ------------------------------------------------------------*/
+
+  /* Exported functions prototypes ---------------------------------------------*/
+
+
+  /* Private defines -----------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
